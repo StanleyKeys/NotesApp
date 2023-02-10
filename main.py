c19d1ecde1d0907f47a504bf5ee76180555f14da
StanleyGunz <stanislavkinsler@gmail.com>
2023-02-10 18:19:08 +0700

    v. 0.33 : created db.json

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import json
+from datetime import date
+import os
 
 
+global dbList
+def checkDB():
+    if os.path.exists('db.json'):
+        createDBlist()
+    else:
+        temp = []
+        file = open('db.json', 'w')
+        file.write(json.dumps(temp, indent=2, ensure_ascii=False))
+        file.close()
 
 print("Добро пожаловать в заметки!\n")
 def startmenu():
+    checkDB()
     print("Выберите действие\n"
           "1. Создать.\n"
           "2. Изменить.\n"
@@ -15,7 +28,7 @@ def startmenu():
 def choosemenu(choice):
     userEnter = choice
     if userEnter == 1:
-        pass
+        createnewnote()
     elif userEnter == 2:
         pass
     elif userEnter == 3:
@@ -28,12 +41,22 @@ def choosemenu(choice):
         print("Нужно выбрать пункт меню")
         startmenu()
 
+def createDBlist():
+    with open("db.json", "r") as file:
+        datb = file.read()
+        dbList = json.loads(datb)
 
 def createnewnote():
     print("Создание заметки.\n")
+    currentDate = f'{date.today()}'
     noteTitle = input("Введите заголовок заметки: ")
     noteBody = input("Введите тело заметки: ")
-
+    data = {'Date': currentDate, 'Title': noteTitle, 'NoteBody': noteBody}
+    loadjson = json.load(open("db.json"))
+    loadjson.append(data)
+    with open("db.json", "w") as file:
+        json.dump(loadjson, file, indent=2, ensure_ascii=False)
     print("Заметка успешно сохранена")
+
 
 startmenu()
--- test.py
+++ test.py
@@ -1,19 +1,49 @@
+from datetime import date
 import json
 
+def createjson():
 
 
 
-tempList = []
 
-data1 = {'Date': "01.01.01", "Title": "Privet", "Body": "Fuck"}
-data2 = {'Date': "12.01.01", "Title": "12", "Body": "2019"}
-data3 = {'Date': "31.01.01", "Title": "31", "Body": "2020"}
 
-tempList.append(data1)
-tempList.append(data2)
-tempList.append(data3)
+    data1 = []
 
-with open('data.json', 'w') as f:
-    json.dump(tempList, f)
+    with open('db.json', 'w') as file:
+        file.write(json.dumps(data1, indent=2, ensure_ascii=False))
 
-print(tempList)
+# data2 = {'Date': "12.01.01", "Title": "12", "Body": "2019"}
+# data3 = {'Date': "31.01.01", "Title": "31", "Body": "2020"}
+#
+# tempList.append(data1)
+# tempList.append(data2)
+# tempList.append(data3)
+
+# with open('data.json', 'w') as f:
+#     json.dump(tempList, f)
+
+def add_to_json():
+    data2 = {'Date': "12.01.01", "Title": "12", "Body": "2019"}
+    data = json.load(open("db.json"))
+    data.append(data2)
+    with open("db.json", "w") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+# add_to_json()
+# createjson()
+# time = date.today()
+# with open('db.json', 'r') as file:
+#     data = file.read()
+#     json_data = json.loads(data)
+#
+# print(json_data[0]['Title'])
+
+
+def createDBlist():
+    with open("db.json", "r") as file:
+        datb = file.read()
+        json_data = json.loads(datb)
+    print(json_data)
+
+
+createDBlist()
